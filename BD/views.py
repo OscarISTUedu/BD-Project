17fa0272656904462f2884b132a41412cdfb5372
OscarISTUedu <f8keakk@gmail.com>
2024-11-27 00:32:51 +0800

    add universal html for models

--- BD/views.py
+++ BD/views.py
@@ -1,7 +1,43 @@
 from django.shortcuts import render
-from .models import Patient
+from .models import Patient, Doctor, Neighborhood, Diagnosis, Visit, Ticket
 
 def patient_list(request):
-    # Получаем все записи пациентов
-    patients = Patient.objects.all()
-    return render(request, 'patient_list.html', {'patients': patients})
+    patients = Patient.objects.all().values()
+    fields = Patient._meta.get_fields()
+    fields = fields[1:]
+    return render(request, 'model_list.html',
+                  {'models': patients,'h1':Patient._meta.verbose_name_plural,'fields':fields})
+
+def doctor_list(request):
+    doctors = Doctor.objects.all().values()
+    fields = Doctor._meta.get_fields()
+    fields = fields[1:]
+    return render(request, 'model_list.html',
+                  {'models': doctors, 'h1': Doctor._meta.verbose_name_plural, 'fields': fields})
+
+def neighborhood_list(request):
+    neighborhoods = Neighborhood.objects.all().values()
+    fields = Neighborhood._meta.get_fields()
+    fields = fields[1:]
+    return render(request, 'model_list.html',
+                  {'models': neighborhoods, 'h1': Neighborhood._meta.verbose_name_plural, 'fields': fields})
+
+def diagnoses_list(request):
+    diagnoses = Diagnosis.objects.all().values()
+    fields = Diagnosis._meta.get_fields()
+    fields = fields[1:]
+    return render(request, 'model_list.html',
+                  {'models': diagnoses, 'h1': Diagnosis._meta.verbose_name_plural, 'fields': fields})
+
+def visit_list(request):
+    visits = Visit.objects.all().values()
+    fields = Visit._meta.get_fields()
+    fields = fields[1:]
+    return render(request, 'model_list.html',
+                  {'models': visits, 'h1': Visit._meta.verbose_name_plural, 'fields': fields})
+
+def ticket(request):
+    tickets = Ticket.objects.all().values()
+    fields = Ticket._meta.get_fields()
+    return render(request, 'model_list.html',
+                  {'models': tickets, 'h1': Ticket._meta.verbose_name_plural, 'fields': fields})
--- BD_Project/urls.py
+++ BD_Project/urls.py
@@ -3,4 +3,9 @@ from BD import views
 
 urlpatterns = [
     path('patients/', views.patient_list, name='patient_list'),
+    path('doctors/', views.doctor_list, name='doctor_list'),
+    path('neighborhoods/', views.neighborhood_list, name='neighborhood_list'),
+    path('diagnoses/', views.diagnoses_list, name='diagnoses_list'),
+    path('visits/', views.visit_list, name='visit_list'),
+    path('tickets/', views.ticket, name='ticket'),
 ]
